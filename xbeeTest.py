4722816adbe6954421a78bc80ce1dbcfce9a6136
BenTheNinjaRock <b.e.n.j.a.m.i.n@hotmail.co.uk>
2017-03-06 17:25:24 +0000

    CSV file generated, added

--- xbeeTest.py
+++ xbeeTest.py
@@ -12,7 +12,7 @@ xbee = XBee(ser)
 #xbee.tx_long_addr(dest_addr=b'\x13\xA2\x00\x41\x54\x53\xBC\xFE')
 a=b'\x10'
 b=b'\x01'
-c=b'\x00\x13\xA2\x00\x41\x54\x53\xD0'
+c=b'\x00\x13\xA2\x00\x41\x54\x53\xC9'
 d=b'\x00'
 t1='t1'
 t2='55'
@@ -25,4 +25,6 @@ e.extend(map(ord, tt))
 
 xbee.tx_long_addr(id=a, frame_id=b, dest_addr=c, options=d, data=e)
 response = xbee.wait_read_frame()
-print(response['status'])
+print(chr(response['status']))
+if response != '\x00':
+    print('error')
