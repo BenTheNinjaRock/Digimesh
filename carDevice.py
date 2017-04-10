70afc17d32f5d282667861392161f08e88ed625a
BenTheNinjaRock <b.e.n.j.a.m.i.n@hotmail.co.uk>
2017-04-10 19:52:55 +0000

    carDevice.py completed, awaiting tests

--- carDevice.py
+++ carDevice.py
@@ -25,28 +25,71 @@ while True:
     data = x['data']
     data = data.split()
     frameType = data[0].decode('utf-8')
+    #If radio receives update
     if frameType == '0':
         #data = 0 TT CC TT:TT:TT
         test = data[1].decode('utf-8')
         car = data[2].decode('utf-8')
         time = data[3].decode('utf-8')
         fieldnames = ['Source', 'Test', 'Car', 'Time']
-        with open('carDevice.csv', 'a') as wf:
+        with open('carDevice.csv', 'a+') as wf:
+            fileText = '1 '
             fileWriter = csv.DictWriter(wf, fieldnames = fieldnames)
-            fileWriter.writerow({'Source': address, 'Test': test, 'Car': car, 'Time': time})
-    #If radio says "send me this record"
+            fileWriter.writerow({'Source': hexAddress, 'Test': test, 'Car': car, 'Time': time})
+            wf.seek(0, 0)
+            fileReader = csv.DictReader(wf, fieldnames = fieldnames)
+            
+            mtuBreaker = 0
+            for row in fileReader:
+                fileText = fileText + row['Test'] + ':' + row['Car'] + ' '
+                #fileArray.append(row['Test'] + ':' + row['Car'])
+                mtuBreaker += 1
+                #Split into multiple packets if the file is longer than the mtu of 42
+                if mtuBreaker == 5:
+                    byteVar = bytearray()
+                    byteVar.extend(map(ord, fileText))
+                    xbee.tx(id=b'\x10', frame_id=b'\x01', dest_addr=b'\x00\x00\x00\x00\x00\x00\xFF\xFF', options=b'\x00', data=byteVar)
+                    mtuBreaker = 0
+                    fileText = '1 '
+            #if a packet of five was not completed, send the remaining
+            if mtuBreaker != 0:
+                byteVar = bytearray()
+                byteVar.extend(map(ord, fileText))
+                xbee.tx(id=b'\x10', frame_id=b'\x01', dest_addr=b'\x00\x00\x00\x00\x00\x00\xFF\xFF', options=b'\x00', data=byteVar)
+
+            #print(fileArray)
+            #TOO LARGE FOR MTU
+
+    #If car radio advertises test/car combos
     elif frameType == '1':
-#        print(bytearray(hexAddress, 'ascii'))
+        testCarRecord = []
+        testCarPacket = []
+        with open('carDevice.csv', 'r') as wf:
+            for line in wf:
+                records = line.split(',')
+                testCarRecord.append([records[1], records[2]])
+
+            for x in data:
+                splitData = x.decode('utf-8')
+                if splitData != '1':
+                    splitData = splitData.split(':')
+                    if splitData not in testCarRecord:
+                        dataVar = '2 ' + splitData[0] + ' ' + splitData[1]
+                        byteVar = bytearray()
+                        byteVar.extend(map(ord, dataVar))
+                        xbee.tx(id=b'\x10', frame_id=b'\x01', dest_addr=address, options=b'\x00', data=byteVar)
+
+    #If radio says "send me this record"
+    elif frameType == '2':
         #data = TT CC
         qTest = data[1].decode('utf-8')
         qCar = data[2].decode('utf-8')
         with open('carDevice.csv', 'r') as wf:
             for line in wf:
-                #records[1] = Test & records[2] = Car
                 records = line.split(',')
                 test = records[1]
                 car = records[2]
-#                print(address)
+                print(hexAddress)
                 if records[1] == qTest and records[2] == qCar:
                     time = records[3]
                     dataVar = '0 ' + test + ' ' + car + ' ' + time
